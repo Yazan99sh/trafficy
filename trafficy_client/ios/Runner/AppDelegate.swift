fcde5258994b4a5273ef492de11a5442dc2c2cfa
yazan99sh <syria.yazan19@gmail.com>
2022-01-01 13:24:36 +0200

    update dependencies

--- trafficy_client/ios/Runner/AppDelegate.swift
+++ trafficy_client/ios/Runner/AppDelegate.swift
@@ -7,7 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCCRinQYP4zlcwOOcPJiWTUTvHc1xvHoyM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
 }
