43510ee34dd84cc24aec8184d01ef35de4f8874d
yazan99sh <syria.yazan19@gmail.com>
2022-01-01 15:56:39 +0200

    update url

--- trafficy_client/lib/consts/urls.dart
+++ trafficy_client/lib/consts/urls.dart
@@ -2,7 +2,7 @@
 
 class Urls {
   static const String APPWRITE_ENDPOINT =
-      'https://9189-81-92-202-22.ngrok.io/v1';
+      'https://1203-109-70-147-158.ngrok.io/v1';
   static const String APPWRITE_PROJECTID = '61a5cebea8348';
   static const String DOMAIN = 'http://138.197.186.138';
   static const String BASE_API = DOMAIN + '';
