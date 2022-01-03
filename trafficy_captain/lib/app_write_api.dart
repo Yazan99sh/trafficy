8874e0c202288d66b58e64079e70e8164e6b3985
yazan99sh <syria.yazan19@gmail.com>
2022-01-03 14:34:03 +0200

    add user

--- trafficy_captain/lib/app_write_api.dart
+++ trafficy_captain/lib/app_write_api.dart
@@ -1,6 +1,8 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:injectable/injectable.dart';
 import 'consts/urls.dart';
+
 @injectable
 class AppwriteApi {
   Future<Client> getClient() async {
@@ -15,6 +17,12 @@ class AppwriteApi {
     var client = await getClient();
     Account account = Account(client);
     return account;
+  }
+
+  Future<User> getUser() async {
+    var account = await getAccount();
+    User user = await account.get();
+    return user;
   }
 
   Future<Database> getDataBase() async {
