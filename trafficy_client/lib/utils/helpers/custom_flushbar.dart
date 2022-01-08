6c400ac5270bc06af56d66028c7e1a9f1bfc5f1b
yazan99sh <syria.yazan19@gmail.com>
2022-01-08 17:40:02 +0200

    update custom flushbar

--- trafficy_client/lib/utils/helpers/custom_flushbar.dart
+++ trafficy_client/lib/utils/helpers/custom_flushbar.dart
@@ -1,6 +1,5 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
-import 'package:trafficy_client/utils/components/fixed_container.dart';
 
 class CustomFlushBarHelper {
   static Flushbar createSuccess(
@@ -12,40 +11,70 @@ class CustomFlushBarHelper {
       EdgeInsets? padding,
       EdgeInsets? margin}) {
     return Flushbar(
-      maxWidth: 600,
-      title: title,
-      message: message,
-      icon: Icon(
-        Icons.info,
-        size: 28.0,
-        color: Colors.white,
-      ),
-      backgroundColor: background ?? Colors.green,
-      duration: Duration(seconds: timeout),
-      borderRadius: BorderRadius.circular(25),
-      flushbarStyle: FlushbarStyle.FLOATING,
-      margin: margin ?? EdgeInsets.all(8),
-      padding: padding ?? EdgeInsets.all(16),
-      flushbarPosition: top ? FlushbarPosition.TOP : FlushbarPosition.BOTTOM,
-    );
+        maxWidth: 600,
+        titleText: Text(
+          title,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        messageText: Text(
+          message,
+          style: const TextStyle(
+              color: Colors.white70, fontWeight: FontWeight.bold),
+        ),
+        icon: const Icon(
+          Icons.info,
+          size: 28.0,
+          color: Colors.white,
+        ),
+        backgroundColor:background ?? Colors.green,
+        duration: Duration(seconds: timeout),
+        flushbarStyle: FlushbarStyle.GROUNDED,
+        flushbarPosition: top ? FlushbarPosition.TOP : FlushbarPosition.BOTTOM,
+        boxShadows: [
+          BoxShadow(
+            color:background ?? Colors.green,
+            offset: const Offset(0.0, 2.0),
+            blurRadius: 5.0,
+          )
+        ]);
   }
 
   static Flushbar createError(
-      {required String title, required String message, int timeout = 4}) {
+      {required String title,
+      required String message,
+      int timeout = 4,
+      Color? background,
+      bool top = true,
+      EdgeInsets? padding,
+      EdgeInsets? margin}) {
     return Flushbar(
-      maxWidth: 600,
-      title: title,
-      message: message,
-      icon: Icon(
-        Icons.info,
-        size: 28.0,
-        color: Colors.white,
-      ),
-      backgroundColor: Colors.red,
-      duration: Duration(seconds: timeout),
-      borderRadius: BorderRadius.circular(25),
-      flushbarStyle: FlushbarStyle.FLOATING,
-      margin: EdgeInsets.all(8),
-    );
+        maxWidth: 600,
+        titleText: Text(
+          title,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        messageText: Text(
+          message,
+          style: const TextStyle(
+              color: Colors.white70, fontWeight: FontWeight.bold),
+        ),
+        icon: const Icon(
+          Icons.info,
+          size: 28.0,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: timeout),
+        flushbarStyle: FlushbarStyle.GROUNDED,
+        flushbarPosition: top ? FlushbarPosition.TOP : FlushbarPosition.BOTTOM,
+        boxShadows: const [
+          BoxShadow(
+            color: Colors.red,
+            offset: Offset(0.0, 2.0),
+            blurRadius: 5.0,
+          )
+        ]);
   }
 }
